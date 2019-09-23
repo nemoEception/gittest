9afcbd754aa948faaf027cfb11559e6b2c9cf718
xcc <lichengCC@sina.com>
2019-09-23 19:43:01 +0800

    修改了hi git程序

--- git_test/src/main/java/HelloWorld.java
+++ git_test/src/main/java/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("hi Git");
         System.out.println("hi Git");
         System.out.println("hi Git");
+        System.out.println("hi Git");
     }
 }
